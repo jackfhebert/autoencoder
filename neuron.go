ff76b26d47aae60e5bd0c3063fe706efb92ec37f
Jack Hebert <jhebert@gmail.com>
2014-01-16 22:54:33 -0800

    closer

--- neuron.go
+++ neuron.go
@@ -5,23 +5,25 @@ A single neuron, can backprop itself to learn weights.
 package AutoEncoder
 
 import (
-       "fmt"
        "math"
        "math/rand"
 )
 
 type Neuron struct {
     alpha float64
+    decay float64
     weights []float64
     inputs chan<- float64
     outputs <-chan float64
 }
 
 func NewNeuron(numInputs int) *Neuron {
-  node := &Neuron{0.01, make([]float64, numInputs), nil, nil}
-  for i:= 0; i < numInputs; i++ {
-    node.weights[i] = rand.Float64() * .5 - 0.5
-    fmt.Println("Init weight to", node.weights[i])
+  // Want the number of weights to be 1 longer than the number
+  // of inputs - this simulates having a bias unit- sorta like
+  // that silly +C back when you did calculus.
+  node := &Neuron{0.05, 0.005, make([]float64, numInputs + 1), nil, nil}
+  for i:= 0; i < len(node.weights); i++ {
+    node.weights[i] = rand.Float64() * .5 - 0.25
   }
   return node
 }
@@ -34,10 +36,14 @@ func (node *Neuron) Process() {
 
 // Compute the activation of this node.
 func (node* Neuron) Predict(input []float64) float64 {
+  // Sum up the weighted activation.
   value := 0.0
-  for i := 0; i < len(node.weights); i++ {
+  // Be careful that node.weights is longer than len(input).
+  for i := 0; i < len(input); i++ {
     value += input[i] * node.weights[i]
   }
+  // Add in the activation for the bias unit.
+  value += 1 * node.weights[len(node.weights) - 1]
 
   return 1.0 / (1 + math.Exp(-value))
 }
@@ -45,12 +51,20 @@ func (node* Neuron) Predict(input []float64) float64 {
 func (node* Neuron) Update(input []float64, result float64) float64 {
   // Figure out what this node would output.
   wouldOutput := node.Predict(input)
-  fmt.Println("Goal", result)
-  fmt.Println("Would ouput", wouldOutput)
+
   // Update the weight per input to get closer to the desired output.
-  for i := 0; i < len(node.weights); i++ {
-    fmt.Println(node.alpha * (result - wouldOutput) * input[i])
-    node.weights[i] += node.alpha * (result - wouldOutput) * input[i]
+  for i := 0; i < len(input); i++ {
+    // This is the gradient of the error function.
+    grad := (wouldOutput - result) * input[i]
+    // Add in a penalty for large weights.
+    decay := node.decay * node.weights[i]
+    node.weights[i] -= node.alpha * (grad + decay)
   }
+
+  // Update the bias node which always has an input value of 1.
+  grad := (wouldOutput - result)
+  decay := node.decay * node.weights[len(node.weights) - 1]
+
+  node.weights[len(node.weights) - 1] -= node.alpha * (grad + decay)
   return result - wouldOutput
 }
