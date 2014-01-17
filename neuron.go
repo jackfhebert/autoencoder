36323027a509270a3f044a0c0b51e910a7d3f4c9
Jack Hebert <jhebert@gmail.com>
2014-01-16 22:59:04 -0800

    formatting

--- neuron.go
+++ neuron.go
@@ -5,66 +5,66 @@ A single neuron, can backprop itself to learn weights.
 package AutoEncoder
 
 import (
-       "math"
-       "math/rand"
+	"math"
+	"math/rand"
 )
 
 type Neuron struct {
-    alpha float64
-    decay float64
-    weights []float64
-    inputs chan<- float64
-    outputs <-chan float64
+	alpha   float64
+	decay   float64
+	weights []float64
+	inputs  chan<- float64
+	outputs <-chan float64
 }
 
 func NewNeuron(numInputs int) *Neuron {
-  // Want the number of weights to be 1 longer than the number
-  // of inputs - this simulates having a bias unit- sorta like
-  // that silly +C back when you did calculus.
-  node := &Neuron{0.05, 0.005, make([]float64, numInputs + 1), nil, nil}
-  for i:= 0; i < len(node.weights); i++ {
-    node.weights[i] = rand.Float64() * .5 - 0.25
-  }
-  return node
+	// Want the number of weights to be 1 longer than the number
+	// of inputs - this simulates having a bias unit- sorta like
+	// that silly +C back when you did calculus.
+	node := &Neuron{0.05, 0.005, make([]float64, numInputs+1), nil, nil}
+	for i := 0; i < len(node.weights); i++ {
+		node.weights[i] = rand.Float64()*.5 - 0.25
+	}
+	return node
 }
 
 func (node *Neuron) Process() {
-  // This should weight for either channel to have a value, but only
-  // if the channels exist.
-  
+	// This should weight for either channel to have a value, but only
+	// if the channels exist.
+
 }
 
 // Compute the activation of this node.
-func (node* Neuron) Predict(input []float64) float64 {
-  // Sum up the weighted activation.
-  value := 0.0
-  // Be careful that node.weights is longer than len(input).
-  for i := 0; i < len(input); i++ {
-    value += input[i] * node.weights[i]
-  }
-  // Add in the activation for the bias unit.
-  value += 1 * node.weights[len(node.weights) - 1]
+func (node *Neuron) Predict(input []float64) float64 {
+	// Sum up the weighted activation.
+	value := 0.0
+	// Be careful that node.weights is longer than len(input).
+	for i := 0; i < len(input); i++ {
+		value += input[i] * node.weights[i]
+	}
+	// Add in the activation for the bias unit.
+	value += 1 * node.weights[len(node.weights)-1]
 
-  return 1.0 / (1 + math.Exp(-value))
+	return 1.0 / (1 + math.Exp(-value))
 }
 
-func (node* Neuron) Update(input []float64, result float64) float64 {
-  // Figure out what this node would output.
-  wouldOutput := node.Predict(input)
+func (node *Neuron) Update(input []float64, result float64) float64 {
+	// Figure out what this node would output.
+	wouldOutput := node.Predict(input)
 
-  // Update the weight per input to get closer to the desired output.
-  for i := 0; i < len(input); i++ {
-    // This is the gradient of the error function.
-    grad := (wouldOutput - result) * input[i]
-    // Add in a penalty for large weights.
-    decay := node.decay * node.weights[i]
-    node.weights[i] -= node.alpha * (grad + decay)
-  }
+	// Update the weight per input to get closer to the desired output.
+	for i := 0; i < len(input); i++ {
+		// This is the gradient of the error function.
+		grad := (wouldOutput - result) * input[i]
+		// Add in a penalty for large weights.
+		decay := node.decay * node.weights[i]
+		node.weights[i] -= node.alpha * (grad + decay)
+	}
 
-  // Update the bias node which always has an input value of 1.
-  grad := (wouldOutput - result)
-  decay := node.decay * node.weights[len(node.weights) - 1]
+	// Update the bias node which always has an input value of 1.
+	grad := (wouldOutput - result)
+	decay := node.decay * node.weights[len(node.weights)-1]
 
-  node.weights[len(node.weights) - 1] -= node.alpha * (grad + decay)
-  return result - wouldOutput
+	node.weights[len(node.weights)-1] -= node.alpha * (grad + decay)
+	return result - wouldOutput
 }
--- neuron_test.go
+++ neuron_test.go
@@ -1,139 +1,139 @@
 package AutoEncoder
 
 import (
-       "fmt"
-       "math/rand"
-       "testing"
+	"fmt"
+	"math/rand"
+	"testing"
 )
 
 // Try to learn the logical OR function over three
 // inputs.
 func Test_LearnOr(t *testing.T) {
-  neuron := NewNeuron(3)
-  inputs := []float64{0, 0, 0}
-  result := 0.0
-  for i := 0; i < 50000; i++ {
-    // Each iteration, randomize the inputs.
-    result = 0
-    for j := 0; j < len(inputs); j++ {
-      if rand.Float64() < .5 {
-        inputs[j] = 0
-      } else {
-        inputs[j] = 1
-        result = 1
-      }
-    }
+	neuron := NewNeuron(3)
+	inputs := []float64{0, 0, 0}
+	result := 0.0
+	for i := 0; i < 50000; i++ {
+		// Each iteration, randomize the inputs.
+		result = 0
+		for j := 0; j < len(inputs); j++ {
+			if rand.Float64() < .5 {
+				inputs[j] = 0
+			} else {
+				inputs[j] = 1
+				result = 1
+			}
+		}
 
-    neuron.Update(inputs, result)
-  }
-  fmt.Println("OR", neuron.weights)
-  inputs = []float64{1, 1, 1}
-  if neuron.Predict(inputs) < .5 {
-    t.Error("failed on all 1's")
-  }
+		neuron.Update(inputs, result)
+	}
+	fmt.Println("OR", neuron.weights)
+	inputs = []float64{1, 1, 1}
+	if neuron.Predict(inputs) < .5 {
+		t.Error("failed on all 1's")
+	}
 
-  inputs = []float64{1, 0, 1}
-  if neuron.Predict(inputs) < .5 {
-    t.Error("failed on 2 1's")
-  }
+	inputs = []float64{1, 0, 1}
+	if neuron.Predict(inputs) < .5 {
+		t.Error("failed on 2 1's")
+	}
 
-  inputs = []float64{0, 1, 1}
-  if neuron.Predict(inputs) < .5 {
-    t.Error("failed on 2 1's")
-  }
+	inputs = []float64{0, 1, 1}
+	if neuron.Predict(inputs) < .5 {
+		t.Error("failed on 2 1's")
+	}
 
-  inputs = []float64{1, 1, 0}
-  if neuron.Predict(inputs) < .5 {
-    t.Error("failed on 2 1's")
-  }
+	inputs = []float64{1, 1, 0}
+	if neuron.Predict(inputs) < .5 {
+		t.Error("failed on 2 1's")
+	}
 
-  inputs = []float64{1, 0, 0}
-  if neuron.Predict(inputs) < .5 {
-    t.Error("failed on 1 1's")
-  }
+	inputs = []float64{1, 0, 0}
+	if neuron.Predict(inputs) < .5 {
+		t.Error("failed on 1 1's")
+	}
 
-  inputs = []float64{0, 1, 0}
-  if neuron.Predict(inputs) < .5 {
-    t.Error("failed on 1 1's")
-  }
+	inputs = []float64{0, 1, 0}
+	if neuron.Predict(inputs) < .5 {
+		t.Error("failed on 1 1's")
+	}
 
-  inputs = []float64{0, 0, 1}
-  if neuron.Predict(inputs) < .5 {
-    t.Error("failed on 1 1's")
-  }
+	inputs = []float64{0, 0, 1}
+	if neuron.Predict(inputs) < .5 {
+		t.Error("failed on 1 1's")
+	}
 
-  inputs = []float64{0, 0, 0}
-  if neuron.Predict(inputs) > .5 {
-    t.Error("failed on 0 1's")
-    fmt.Println(neuron.weights)
-    fmt.Println(neuron.Predict(inputs))
-  }
+	inputs = []float64{0, 0, 0}
+	if neuron.Predict(inputs) > .5 {
+		t.Error("failed on 0 1's")
+		fmt.Println(neuron.weights)
+		fmt.Println(neuron.Predict(inputs))
+	}
 }
 
 // Try to learn the logical AND over three inputs.
 func Test_LearnAnd(t *testing.T) {
-  neuron := NewNeuron(3)
-  inputs := []float64{0, 0, 0}
-  result := 0.0
-  for i := 0; i < 50000; i++ {
-    // Each iteration, randomize the inputs.
-    result = 1
-    for j := 0; j < len(inputs); j++ {
-      if rand.Float64() < .5 {
-        inputs[j] = 0
-        result = 0
-      } else {
-        inputs[j] = 1
-      }
-    }
+	neuron := NewNeuron(3)
+	inputs := []float64{0, 0, 0}
+	result := 0.0
+	for i := 0; i < 50000; i++ {
+		// Each iteration, randomize the inputs.
+		result = 1
+		for j := 0; j < len(inputs); j++ {
+			if rand.Float64() < .5 {
+				inputs[j] = 0
+				result = 0
+			} else {
+				inputs[j] = 1
+			}
+		}
 
-    neuron.Update(inputs, result)
-  }
-  fmt.Println("AND", neuron.weights)
- 
- inputs = []float64{1, 1, 1}
-  if neuron.Predict(inputs) < .5 {
-    t.Error("failed on all 1's")
-    fmt.Println(neuron.weights)
-  }
+		neuron.Update(inputs, result)
+	}
+	fmt.Println("AND", neuron.weights)
 
-  inputs = []float64{1, 0, 1}
-  if neuron.Predict(inputs) > .5 {
-    t.Error("failed on 2 1's")
-    fmt.Println(neuron.weights)
-  }
+	inputs = []float64{1, 1, 1}
+	if neuron.Predict(inputs) < .5 {
+		t.Error("failed on all 1's")
+		fmt.Println(neuron.weights)
+	}
 
-  inputs = []float64{0, 1, 1}
-  if neuron.Predict(inputs) > .5 {
-    t.Error("failed on 2 1's")
-    fmt.Println(neuron.weights)
-  }
+	inputs = []float64{1, 0, 1}
+	if neuron.Predict(inputs) > .5 {
+		t.Error("failed on 2 1's")
+		fmt.Println(neuron.weights)
+	}
 
-  inputs = []float64{1, 1, 0}
-  if neuron.Predict(inputs) > .5 {
-    t.Error("failed on 2 1's")
-    fmt.Println(neuron.weights)
-  }
+	inputs = []float64{0, 1, 1}
+	if neuron.Predict(inputs) > .5 {
+		t.Error("failed on 2 1's")
+		fmt.Println(neuron.weights)
+	}
 
-  inputs = []float64{1, 0, 0}
-  if neuron.Predict(inputs) > .5 {
-    t.Error("failed on 1 1's")
-    fmt.Println(neuron.weights)
-  }
+	inputs = []float64{1, 1, 0}
+	if neuron.Predict(inputs) > .5 {
+		t.Error("failed on 2 1's")
+		fmt.Println(neuron.weights)
+	}
 
-  inputs = []float64{0, 1, 0}
-  if neuron.Predict(inputs) > .5 {
-    t.Error("failed on 1 1's")
-  }
+	inputs = []float64{1, 0, 0}
+	if neuron.Predict(inputs) > .5 {
+		t.Error("failed on 1 1's")
+		fmt.Println(neuron.weights)
+	}
 
-  inputs = []float64{0, 0, 1}
-  if neuron.Predict(inputs) > .5 {
-    t.Error("failed on 1 1's")
-  }
+	inputs = []float64{0, 1, 0}
+	if neuron.Predict(inputs) > .5 {
+		t.Error("failed on 1 1's")
+	}
 
-  inputs = []float64{0, 0, 0}
-  if neuron.Predict(inputs) > .5 {
-    t.Error("failed on 0 1's")
-  }
+	inputs = []float64{0, 0, 1}
+	if neuron.Predict(inputs) > .5 {
+		t.Error("failed on 1 1's")
+	}
+
+	inputs = []float64{0, 0, 0}
+	if neuron.Predict(inputs) > .5 {
+		t.Error("failed on 0 1's")
+	}
 
 }
